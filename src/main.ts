134c363fb8799df1bd680af0c1c2bb60def76fd4
Alex Warrington <awarrington0895@gmail.com>
2022-03-20 17:22:37 -0500

    Create root api

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
-console.log('test');
+import { createServer, httpListener, r } from '@marblejs/http';
+import { bodyParser$ } from '@marblejs/middleware-body';
+import { logger$ } from '@marblejs/middleware-logger';
+import { IO } from 'fp-ts/lib/IO';
+import { map } from 'rxjs/operators';
+
+const api$ = r.pipe(
+  r.matchPath('/'),
+  r.matchType('GET'),
+  r.useEffect(req$ => req$.pipe(
+    map(() => ({ body: 'Hello, world!' }))
+  ))
+);
+
+const middlewares = [
+  logger$(),
+  bodyParser$()
+];
+
+const effects = [
+  api$
+];
+
+const listener = httpListener({
+  middlewares,
+  effects
+});
+
+const server = createServer({
+  port: 1337,
+  hostname: '127.0.0.1',
+  listener
+});
+
+const main: IO<void> = async () => await (await server)();
+
+main();
