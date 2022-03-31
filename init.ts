ff56aba172bb48d406db7fd1dbdd77db4d930deb
Alex <awarrington0895@gmail.com>
2022-03-31 07:51:33 -0500

    Feature/tweak gitpod setup (#1)
    * Make necessary tweaks
    
    * Add httpie to dockerfile
    
    * Set port defaults and add gitconfig aliases
    
    * Add a few more extensions to gitpod.yml

--- init.ts
+++ init.ts
@@ -4,9 +4,9 @@ import { execSync, exec } from "child_process";
 const execP = promisify(exec);
 
 Promise.all([
-  execP("npm install"),
   execP("docker-compose up -d"),
   execP("cat .env.sample > .env"),
 ]).then(() => {
+  execSync("gp await-port 5432");
   execSync("npm run db:migrate");
 });
